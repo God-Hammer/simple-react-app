b5ec315596fc33171f9a82ca4e094785db1f9495
Tấn Trung <nguyentantrung1801@gmail.com>
2024-01-10 21:44:31 +0700

    add like button

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,59 @@
 import { useState } from "react";
 import "./App.css";
-import Button from "./component/Button";
+import Button from "./component/button/Button";
 import Alert from "./component/Alert";
+import { Like } from "./component/Like";
+import Message from "./Message";
 
 function App() {
   const [alertVisiable, setAlertVisiable] = useState(false);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "Trung",
+    },
+  });
+  const [pizza, setPizza] = useState({
+    name: "Phomai",
+    toppings: ["Thit", "Bo"],
+  });
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
+
+  const handleClick = () => {
+    setGame({ ...game, player: { ...game.player, name: "Hammer" } });
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "Heo"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+  };
+  console.log("Cart", cart.items);
   return (
     <>
+      <div>
+        <Message />
+      </div>
+
       <div>
         {alertVisiable && (
           <Alert onClose={() => setAlertVisiable(false)}>My alert</Alert>
         )}
-        <Button onClick={() => setAlertVisiable(true)} color="danger">
+        <Button onClick={() => setAlertVisiable(true)} color="primary">
           My button
         </Button>
+
+        <Like onClick={() => console.log("Click ne")} />
+      </div>
+      <div>
+        <button onClick={handleClick}>Show</button>
       </div>
     </>
   );
--- src/component/button/Button.tsx
+++ src/component/button/Button.tsx
@@ -1,3 +1,5 @@
+import styles from "./Button.module.css";
+
 interface Props {
   children: string;
   color?: "primary" | "secondary" | "danger";
@@ -6,7 +8,11 @@ interface Props {
 
 const Button = ({ children, color = "primary", onClick }: Props) => {
   return (
-    <div className={"btn btn-" + color} onClick={onClick}>
+    // <div className={"btn btn-" + color} onClick={onClick}>
+    <div
+      className={[styles.btn, styles["btn-" + color]].join(" ")}
+      onClick={onClick}
+    >
       {children}
     </div>
   );
